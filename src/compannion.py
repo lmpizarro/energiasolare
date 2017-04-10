8ad673cf37ef319ea5559f573f3a2888d176a237
lmpizarro <lmpizarro@gmail.com>
2017-04-10 12:13:17 -0300

    init companion.py opencircuit

--- src/compannion.py
+++ src/compannion.py
@@ -6,22 +6,45 @@ import constants
 
 Gp = 1.0 / 116.140
 I0 = 5.689E-14
-nref = 0.74
+nref = 0.815
 Iirr = 8.51
 Ns = 60
 T = 300
 
 
 def openCircuit():
-    str1 = ("Gp: %.3f, I0: %.3e, nref: %.3f, Iirr: %.3f, Ns: %.3f")%(Gp, I0, nref, Iirr, Ns) 
+    Vt = Ns * nref * T  * constants.KK / constants.qq
+
+    str1 = ("Gp: %.3f, I0: %.3e, nref: %.3f, Iirr: %.3f, Ns: %.3f Vt: %.3f")% \
+            (Gp, I0, nref, Iirr, Ns, Vt) 
+    
     print str1
 
-    Gd = I0 * constants.qq / (constants.KK * nref * Ns * T )
 
+    vd = 60
     id_ = 0.0
-    v1 = Iirr / (Gd + Gp)
 
-    print Gd, id_, v1
+    Gd = (I0 / Vt) * math.exp(vd / Vt)
+
+    v1 = (Iirr / 1.00 - (id_ - Gd * vd)) / (Gd + Gp)
+
+    print ("Gd: %.3e id: %.3e v1: %.3e")%(Gd, id_, v1)
+
+    for i in range(1000):
+        err = vd
+        #id_ = id_ + Gd * (v1 - vd)
+        id_ = I0 * (math.exp(vd / Vt))
+        v1 = (Iirr /1.00 - (id_ - Gd * vd)) / (Gd + Gp)
+        vd = v1
+        Gd = (I0 / Vt) * math.exp(vd / Vt)
+        err = err - vd
+        
+        if math.fabs(err) < 0.0001:
+            break
+
+        print ("Gd: %.3e id: %.3e v1: %.3e err:%.3e")%(Gd, id_, v1, err)
+
+    print i        
 
 if __name__ == '__main__':
     openCircuit()
