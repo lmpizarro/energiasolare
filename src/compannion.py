51820d7356c2f1eb4d9384d79fd703e183f4b54b
lmpizarro <lmpizarro@gmail.com>
2017-04-11 13:25:54 -0300

    companion cont

--- src/compannion.py
+++ src/compannion.py
@@ -5,125 +5,6 @@ import math
 import constants
 import bdModulos
 
-Gp = 1.0 / 62.5481
-Gs = 1.0 / 0.733
-
-
-I0 = 9.84-42
-nref = 0.25
-Iirr = 8.5794
-Ns = 60
-T = 300
-Vt = Ns * nref * T  * constants.KK / constants.qq
-Vmpp = 29.6
-Impp = 7.94
-Gmpp = Impp / Vmpp
-Isc = 8.4
-Voc = 36.8
-
-str1 = ("Gmpp %.3f Gp: %.3f, I0: %.3e, nref: %.3f, Iirr: %.3f, Ns: %.3f Vt: %.3f")% \
-        (Gmpp, Gp, I0, nref, Iirr, Ns, Vt) 
-    
-#print str1
-
-vd = 10.0
-id_ = 0.0
-Gd = (I0 / Vt) * math.exp(vd / Vt)
-
-
-#print ("Gd: %.3e id: %.3e v1: %.3e \n")%(Gd, id_, v1)
-
-
-def mppCircuit():
-    global vd, Gd, id_
-
-    gl = Gs * Gmpp / (Gs + Gmpp)
-
-
-    v1 = (Iirr - (id_ - Gd * vd)) / (Gd + Gp + gl)
-
-    for i in range(1000):
-        err = vd
-        id_ = I0 * (math.exp(vd / Vt) -  1)
-        v1 = (Iirr - (id_ - Gd * vd)) / (Gd + Gp + gl)
-        vd = v1
-        Gd = (I0 / Vt) * math.exp(vd / Vt)
-        err = err - vd
-        il = vd * gl
-        
-        if math.fabs(err) < 0.0001:
-            break
-
-    print ("MPP Gd: %.3e id: %.3e v1: %.3e il: %.3e err:%.3e")%(Gd, id_, v1, il, err)
-
-    diffV = 100 * (Vmpp - v1) / Vmpp
-    diffI = 100 * (Impp - il)/ Impp
-
-    print ("error calc: %.3e error pc Vmp: %.3f error pc Imp: %.3f")%(err, diffV, diffI)
-
-
-
-def shortCircuit():
-   
-    global vd, Gd, id_
-
-    v1 = (Iirr / 1.00 - (id_ - Gd * vd)) / (Gd + Gp + Gs)
-    for i in range(1000):
-        err = vd
-        id_ = I0 * (math.exp(vd / Vt) - 1)
-        v1 = (Iirr - (id_ - Gd * vd)) / (Gd + Gp + Gs)
-        vd = v1
-        Gd = (I0 / Vt) * math.exp(vd / Vt)
-        err = err - vd
-        il = vd * Gs
-        
-        if math.fabs(err) < 0.0001:
-            break
-
-
-    print ("SHORT Gd: %.3e id: %.3e v1: %.3e il: %.3e err:%.3e")%(Gd, id_, v1, il, err)
-
-    diff = Isc - il 
-
-    print diff
-
-
-
-def openCircuit():
-
-    #global vd, Gd, id_
-
-    vd = 60.0
-    id_ = 0.0
-
-    Gd = (I0 / Vt) * math.exp(vd / Vt)
- 
-
-    v1 = (Iirr / 1.00 - (id_ - Gd * vd)) / (Gd + Gp)
-
-    for i in range(1000):
-        err = vd
-        id_ = I0 * (math.exp(vd / Vt) - 1)
-        v1 = (Iirr - (id_ - Gd * vd)) / (Gd + Gp)
-        vd = v1
-        Gd = (I0 / Vt) * math.exp(vd / Vt)
-        err = err - vd
-        
-        if math.fabs(err) < 0.0001:
-            break
-
-
-    print ("OPEN Gd: %.3e id: %.3e v1: %.3e err:%.3e")%(Gd, id_, v1, err)
-
-    diff = Voc - v1
-
-    print diff
-
-
-def test01 ():
-    mppCircuit()
-    shortCircuit()
-    openCircuit()
 
 class Companion(object):
     def __init__(self, modulo, index, Temp):
@@ -216,14 +97,16 @@ class Companion(object):
         return str1 + str2
 
 def testCompanion():
-    indexModel = 1
-    T = 25
-    mod1 = Companion(bdModulos.eschedaTecnica4, indexModel, T)
-    print mod1
 
-    mod1.mppCircuit(30, "mpp")
-    mod1.mppCircuit(30, "open")
-    mod1.mppCircuit(30, "short")
+    indexModel = 4
+    T = 25
+
+    for i in range(5):
+        indexModel = i
+        mod1 = Companion(bdModulos.eschedaTecnica4, indexModel, T)
+        mod1.mppCircuit(40, "mpp")
+        mod1.mppCircuit(40, "open")
+        mod1.mppCircuit(40, "short")
 
 
 if __name__ == '__main__':
