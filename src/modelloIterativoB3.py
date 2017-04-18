7b72d34dbd9f99fd45f0d574abf1bc2b3f566b69
lmpizarro <lmpizarro@gmail.com>
2017-04-18 17:26:22 -0300

    modello iterativo B3 cont

--- src/modelloIterativoB3.py
+++ src/modelloIterativoB3.py
@@ -26,13 +26,13 @@ class modelloB3(object):
 
         self.Vt = self.model.Ns * self.nref * constants.KK * self.T / constants.qq
 
-        self.I0 = self.setI0()
+        self.I0 = self.calcI0()
 
         m.esp["modelli"] = []
 
         
-    def setI0(self):
-        return (self.Ii - self.model.Voc / self.Rsh) / (math.exp(self.model.Voc /self.Vt) - 1 )
+    def calcI0(self):
+        return (self.Ii - (self.model.Voc / self.Rsh)) / (math.exp(self.model.Voc /self.Vt) - 1 )
 
     def calcCurrrent(self, I, V):
         Ii = self.Ii
@@ -68,7 +68,7 @@ class modelloB3(object):
             while deltaIsc > 0.0001:
                 self.cIi = self.cIi + 0.00001
                 self.Ii = self.cIi * self.model.Isc
-                self.I0 = self.setI0()
+                self.I0 = self.calcI0()
                 deltaIsc = self.calcDeltaIsc()
 
         if deltaIsc < -0.0001:
@@ -76,39 +76,56 @@ class modelloB3(object):
             while deltaIsc < -0.0001:
                 self.cIi = self.cIi - 0.00001
                 self.Ii = self.cIi * self.model.Isc
-                self.I0 = self.setI0()
+                self.I0 = self.calcI0()
                 deltaIsc = self.calcDeltaIsc()
 
 
     def ajustaVoc(self):
-        deltaVoc =  self.calcDeltaVoc()   
+        deltaVoc =  self.calcDeltaVoc() 
+        print "deltaVOC init", deltaVoc
 
-        if deltaVoc < 0:
-            print "deltaVoc < 0: ", deltaVoc
-            while deltaVoc < 0:
+        if deltaVoc < -0.0001:
+            print "deltaVoc < -0.0001: ", deltaVoc
+            while deltaVoc < -0.0001:
                 self.nref = self.nref - 0.0001
                 self.Vt = self.model.Ns * self.nref * constants.KK * self.T / constants.qq
-                self.I0 = self.setI0()
-                self.ajustaIsc()
+                self.I0 = self.calcI0()
+                #self.ajustaIsc()
                 deltaVoc =  self.calcDeltaVoc()   
+
+        if deltaVoc > 0.0001:
+            print "deltaVoc > 0.0001: ", deltaVoc
+            while deltaVoc > 0.0001:
+                self.nref = self.nref + 0.0001
+                self.Vt = self.model.Ns * self.nref * constants.KK * self.T / constants.qq
+                self.I0 = self.calcI0()
+                #self.ajustaIsc()
+                deltaVoc =  self.calcDeltaVoc()   
+
+
+
+
 
     def ajustaMPP(self):
         deltaMPP = self.calcDeltaImp()
 
         if deltaMPP > 0.0:
+            print "deltaMPP > 0", deltaMPP 
             while deltaMPP > 0:
                 #print "deltaMPP > 0", deltaMPP 
                 self.Rs = self.Rs - 0.001 
                 self.Rsh = self.Rsh +   .1
-
+                self.I0 = self.calcI0()
+                #self.ajustaVoc()
                 deltaMPP = self.calcDeltaImp()
 
     def ajusta (self):
         self.ajustaIsc()
+        self.ajustaMPP()
         self.ajustaVoc()
         self.ajustaMPP()
 
-        self.ajustaVoc()
+        #self.ajustaVoc()
         self.model.esp["modelli"].append({"Rs":self.Rs, "Rsh": self.Rsh, "nref": self.nref,\
                 "source": "B2", "Iirr": self.Ii, "I0": self.I0})
 
@@ -116,8 +133,6 @@ class modelloB3(object):
         str1 = ("Rsh: %.3f Rs: %.3f nref: %.3f Ii: %.3f I0: %.3e\n")%\
                 (self.Rsh, self.Rs, self.nref, self.Ii, self.I0)
         return str1
-
-
 
 def testB3():
     Ta = 30
@@ -132,6 +147,7 @@ def testB3():
     mbcMd3 = modelloB3(md1, ambient)
     print(mbcMd3)
 
+    mbcMd3.ajusta()
     mbcMd3.ajusta()
 
     print(mbcMd3)
