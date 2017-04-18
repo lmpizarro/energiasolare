4588806594716a4d2fb45502a6c6067c3432feb5
lmpizarro <lmpizarro@gmail.com>
2017-04-18 10:09:25 -0300

    modello iterativo B3 init

--- src/fvMmodel.py
+++ src/fvMmodel.py
@@ -15,114 +15,6 @@ import bdModulos
 """
 
 
-class modelloB3(object):
-    def __init__(self, m, ambient):
-        self.model = m
-
-        deltav = m.Voc - m.Vmp
-        deltai = m.Isc - m.Imp
-
-        self.cRs = 1
-        self.cRsh = 2.0
-        self.cIi = 1.00
-
-        self.T =  ambient.Ta + 273.16
-
-        self.Rs = (deltav / self.model.Imp) / self.cRs  
-
-        self.Rsh = self.cRsh * self.model.Voc / deltai
-
-        self.Ii = self.cIi * self.model.Isc
-
-        self.nref = 1.0
-
-        self.cI0 = 1.0
-
-        self.Vt = self.model.Ns * self.nref * constants.KK * self.T / constants.qq
-
-        self.I0 = self.setI0()
-
-        m.esp["modelli"] = []
-
-        
-        m.esp["modelli"].append({"Rs":self.Rs, "Rsh": self.Rsh, "nref": self.nref,\
-                "source": "B2", "Iirr": self.Ii, "I0": self.I0})
- 
-        
-
-    def setI0(self):
-        return self.cI0 * (self.Ii - self.model.Voc / self.Rsh) / (math.exp(self.model.Voc /self.Vt) - 1 )
-
-    def __str__(self):    
-        str1 = ("Rsh: %.3f Rs: %.3f nref: %.3f Ii: %.3f I0: %.3e\n")%\
-                (self.Rsh, self.Rs,self.nref, self.Ii, self.I0)
-        return str1
-
-    def calcDeltaIsc (self):
-        return self.model.Isc  - (self.Ii - self.I0 * (math.exp(self.model.Isc * self.Rs / self.Vt) - 1) - \
-                self.model.Isc * self.Rs / self.Rsh)
-
-    def calcDeltaVoc (self):
-        return self.model.Voc  - self.Rsh * (self.Ii - self.I0 *\
-                (math.exp(self.model.Voc/self.Vt)-1))
-
-    def calcCurrrent(self, I, V):
-        Ii = self.Ii
-        I0 = self.I0
-        Rs = self.Rs
-        Rsh = self.Rsh
-
-        Vt = self.Vt
-
-
-        return Ii - I0 * (math.exp((V + I * Rs) / Vt) - 1) - (V + I * Rs) / Rsh
-
-
-    def calcDeltaImp (self):
-        Imp = self.model.Imp
-        Vmp = self.model.Vmp
-
-
-        return self.model.Imp  - self.calcCurrrent(Imp, Vmp) 
-
-
-    def ajustaIsc(self):
-
-        deltaIsc = self.calcDeltaIsc()
-        
-        i = 0
-        if deltaIsc > 0.0001:
-            print "deltaIsc > 0.0001"
-            while deltaIsc > 0.0001:
-                self.cIi = self.cIi + 0.00001
-                self.I0 = self.setI0()
-                self.Ii = self.cIi * self.model.Isc
-                deltaIsc = self.calcDeltaIsc()
-
-        if deltaIsc < -0.0001:
-            print "deltaIsc < -0.0001"
-            while deltaIsc < -0.0001:
-                self.cIi = self.cIi - 0.00001
-                self.I0 = self.setI0()
-                self.Ii = self.cIi * self.model.Isc
-                deltaIsc = self.calcDeltaIsc()
-
-
-
-    def ajustaVoc(self):
-        deltaVoc =  self.calcDeltaVoc()   
-
-        if deltaVoc < 0:
-            print deltaVoc
-            while deltaVoc < 0:
-                self.nref = self.nref - 0.0001
-                self.Vt = self.model.Ns * self.nref * constants.KK * self.T / constants.qq
-                self.I0 = self.setI0()
-                self.ajustaIsc()
-                deltaVoc =  self.calcDeltaVoc()   
-
-        print "deltaImp", self.calcDeltaImp()
-
 class modelloB2(object):
     def __init__(self, m, ambient):
 
@@ -437,25 +329,6 @@ class OneDiodeModel(object):
         self.Iph = Iph
 
 
-def testB3():
-    Ta = 30
-    Ws = 4
-    Ss = 1500
-
-    ambient = Ambient (Ta, Ss, Ws)
-
-    md1 = Modulo(bdModulos.eschedaTecnica4)
-
-    print (md1)
-    mbcMd3 = modelloB3(md1, ambient)
-    print(mbcMd3)
-
-    mbcMd3.ajustaIsc()
-    mbcMd3.ajustaVoc()
-    mbcMd3.ajustaIsc()
-
-    print(mbcMd3)
-
 
 def testB2():
 
@@ -540,4 +413,4 @@ def testOneDiodeModel():
     print(md4)        
 
 if __name__ == '__main__':
-    testB3()
+    testB2()
---
+++ src/modelloIterativoB3.py
@@ -0,0 +1,140 @@
+import math
+import fvMmodel as fvm
+import bdModulos
+import constants
+
+class modelloB3(object):
+    def __init__(self, m, ambient):
+        self.model = m
+
+        deltav = m.Voc - m.Vmp
+        deltai = m.Isc - m.Imp
+
+        self.cRs = 1
+        self.cRsh = 1.0
+        self.cIi = 1.00
+
+        self.T =  ambient.Ta + 273.16
+
+        self.Rs = (deltav / self.model.Imp) / self.cRs  
+
+        self.Rsh = self.cRsh * self.model.Voc / deltai
+
+        self.Ii = self.cIi * self.model.Isc
+
+        self.nref = 1.0
+
+        self.cI0 = 1.0
+
+        self.Vt = self.model.Ns * self.nref * constants.KK * self.T / constants.qq
+
+        self.I0 = self.setI0()
+
+        m.esp["modelli"] = []
+
+        
+        m.esp["modelli"].append({"Rs":self.Rs, "Rsh": self.Rsh, "nref": self.nref,\
+                "source": "B2", "Iirr": self.Ii, "I0": self.I0})
+
+    def setI0(self):
+        return self.cI0 * (self.Ii - self.model.Voc / self.Rsh) / (math.exp(self.model.Voc /self.Vt) - 1 )
+
+    def calcCurrrent(self, I, V):
+        Ii = self.Ii
+        I0 = self.I0
+        Rs = self.Rs
+        Rsh = self.Rsh
+
+        Vt = self.Vt
+
+        return Ii - I0 * (math.exp((V + I * Rs) / Vt) - 1) - (V + I * Rs) / Rsh
+
+    def calcDeltaIsc (self):
+        return self.model.Isc  - (self.Ii - self.I0 * (math.exp(self.model.Isc * self.Rs / self.Vt) - 1) - \
+                self.model.Isc * self.Rs / self.Rsh)
+
+    def calcDeltaVoc (self):
+        return self.model.Voc  - self.Rsh * (self.Ii - self.I0 *\
+                (math.exp(self.model.Voc/self.Vt)-1))
+
+    def calcDeltaImp (self):
+        Imp = self.model.Imp
+        Vmp = self.model.Vmp
+
+        return self.model.Imp  - self.calcCurrrent(Imp, Vmp) 
+
+    def ajustaIsc(self):
+
+        deltaIsc = self.calcDeltaIsc()
+        
+        i = 0
+        if deltaIsc > 0.0001:
+            print "deltaIsc > 0.0001"
+            while deltaIsc > 0.0001:
+                self.cIi = self.cIi + 0.00001
+                self.Ii = self.cIi * self.model.Isc
+                self.I0 = self.setI0()
+                deltaIsc = self.calcDeltaIsc()
+
+        if deltaIsc < -0.0001:
+            print "deltaIsc < -0.0001"
+            while deltaIsc < -0.0001:
+                self.cIi = self.cIi - 0.00001
+                self.Ii = self.cIi * self.model.Isc
+                self.I0 = self.setI0()
+                deltaIsc = self.calcDeltaIsc()
+
+
+    def ajustaVoc(self):
+        deltaVoc =  self.calcDeltaVoc()   
+
+        if deltaVoc < 0:
+            print "deltaVoc < 0: ", deltaVoc
+            while deltaVoc < 0:
+                self.nref = self.nref - 0.0001
+                self.Vt = self.model.Ns * self.nref * constants.KK * self.T / constants.qq
+                self.I0 = self.setI0()
+                self.ajustaIsc()
+                deltaVoc =  self.calcDeltaVoc()   
+
+    def ajustaMPP(self):
+        deltaMPP = self.calcDeltaImp()
+
+        if deltaMPP > 0.0:
+            while deltaMPP > 0:
+                #print "deltaMPP > 0", deltaMPP 
+                self.Rs = self.Rs - 0.001 
+                self.Rsh = self.Rsh +   .1
+
+                deltaMPP = self.calcDeltaImp()
+
+    def __str__(self):    
+        str1 = ("Rsh: %.3f Rs: %.3f nref: %.3f Ii: %.3f I0: %.3e\n")%\
+                (self.Rsh, self.Rs, self.nref, self.Ii, self.I0)
+        return str1
+
+
+
+def testB3():
+    Ta = 30
+    Ws = 4
+    Ss = 1500
+
+    ambient = fvm.Ambient (Ta, Ss, Ws)
+
+    md1 = fvm.Modulo(bdModulos.eschedaTecnica4)
+
+    print (md1)
+    mbcMd3 = modelloB3(md1, ambient)
+    print(mbcMd3)
+
+    mbcMd3.ajustaIsc()
+    mbcMd3.ajustaVoc()
+    #mbcMd3.ajustaIsc()
+    mbcMd3.ajustaMPP()
+
+    print(mbcMd3)
+
+
+if __name__ == '__main__':
+    testB3()
